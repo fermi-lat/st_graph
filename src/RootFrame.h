a59f7dd6362b51670139e020913ebdd94c041671
peachey <>
2004-09-03 14:25:30 +0000

    Consolidate IPlotFrame and IPlot into IFrame. Track frames in container in RootFrame, rather than using Engine to track them all.

--- src/RootFrame.h
+++ src/RootFrame.h
@@ -5,13 +5,13 @@
 #ifndef st_graph_RootFrame_h
 #define st_graph_RootFrame_h
 
+#include <list>
+
 #include "st_graph/IFrame.h"
 
 class TGFrame;
 
 namespace st_graph {
-
-  class RootEngine;
 
   /** \class RootFrame
       \brief Interface for base class frame for all graphical frames.
@@ -21,7 +21,7 @@ namespace st_graph {
       static RootFrame * ancestor();
 
       // Construct a RootFrame which encapsulates the given Root TGFrame.
-      RootFrame(RootEngine * engine, IFrame * parent, TGFrame * frame);
+      RootFrame(IFrame * parent, TGFrame * frame);
 
       /// \brief Destruct the frame.
       virtual ~RootFrame();
@@ -32,15 +32,43 @@ namespace st_graph {
       /// \brief Hide this frame and all it contains.
       virtual void unDisplay();
 
+      /** \brief Add the given (sub) frame to this container frame.
+          \param frame The frame being added.
+      */
+      virtual void addFrame(IFrame * frame);
+
+      /** \brief Remove the given (sub) frame to this container frame. If the frame is not currently in the container,
+                 no harm done.
+          \param frame The frame being removed.
+      */
+      virtual void removeFrame(IFrame * frame);
+
+      /// \brief Get the X position of the left edge of the frame.
+      virtual long getL() const;
+
+      /** \brief Set the X position of the left edge of the frame.
+          \param l The new position of the left edge.
+      */
+      virtual void setL(long l);
+
+      /// \brief Get the X position of the right edge of the frame.
+      virtual long getR() const;
+
+      /** \brief Set the X position of the left edge of the frame.
+          \param l The new position of the left edge.
+      */
+      virtual void setR(long r);
+
       /// \brief Get underlying Root frame. Not part of the API.
       virtual TGFrame * getTGFrame();
 
     protected:
-      RootEngine * m_engine;
+      std::list<IFrame *> m_subframes;
       RootFrame * m_parent;
       TGFrame * m_frame;
 
     private:
+      // Constructs a frame without any parents. This is a singleton.
       RootFrame();
   };
 
