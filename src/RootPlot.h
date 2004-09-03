2abae4b309bf602f0e59feafac0803c8e4c84fcc
peachey <>
2004-09-03 14:26:34 +0000

    Derive from IFrame, not IPlot. Add support for histograms as well as scatter plots.

--- src/RootPlot.h
+++ src/RootPlot.h
@@ -1,21 +1,35 @@
+/** \file RootPlot.h
+    \brief Interface of RootPlot class.
+    \author James Peachey, HEASARC/GSSC
+*/
 #ifndef st_graph_RootPlot_h
 #define st_graph_RootPlot_h
 
 #include <string>
 
-#include "st_graph/IPlot.h"
+#include "st_graph/IFrame.h"
+#include "st_graph/ValueSet.h"
 
 class TCanvas;
 class TGraph;
+class TH1;
 
 namespace st_graph {
 
-  class IPlotFrame;
-  class ValueSet;
-
-  class RootPlot : public IPlot {
+  /** \class RootPlot
+      \brief A Root implementation of the IFrame interface. This specific class displays a plot in its frame.
+  */
+  class RootPlot : public IFrame {
     public:
-      RootPlot(IPlotFrame * parent, const std::string & style, const std::string & title, const ValueSet & x, const ValueSet & y,
+      /** \brief Construct a RootPlot object.
+          \param parent The parent frame.
+          \param style The style of the plot. Currently, either hist* or scat*, case insensitive, will produce
+                 a histogram or scatter plot, respectively.
+          \param x The first dimension.
+          \param y The second dimension.
+          \param z The third dimension.
+      */
+      RootPlot(IFrame * parent, const std::string & style, const std::string & title, const ValueSet & x, const ValueSet & y,
         const ValueSet & z = ValueSet());
 
       virtual ~RootPlot();
@@ -26,15 +40,59 @@ namespace st_graph {
       /// \brief Hide this plot.
       virtual void unDisplay();
 
+      /** \brief Add the given (sub) frame to this container frame.
+          \param frame The frame being added.
+      */
+      virtual void addFrame(IFrame * frame);
+
+      /** \brief Remove the given (sub) frame to this container frame. If the frame is not currently in the container,
+                 no harm done.
+          \param frame The frame being removed.
+      */
+      virtual void removeFrame(IFrame * frame);
+
+      /// \brief Get the X position of the left edge of the frame.
+      virtual long getL() const;
+
+      /** \brief Set the X position of the left edge of the frame.
+          \param l The new position of the left edge.
+      */
+      virtual void setL(long l);
+
+      /// \brief Get the X position of the right edge of the frame.
+      virtual long getR() const;
+
+      /** \brief Set the X position of the left edge of the frame.
+          \param l The new position of the left edge.
+      */
+      virtual void setR(long r);
+
       /** \brief Associate this plot with the given canvas. Not API call.
           \param canvas The canvas.
       */
       virtual void setCanvas(TCanvas * canvas);
 
+      /** \brief Create histogram plot as a Root object. Not part of API.
+          \param title The plot title.
+          \param x The first dimension.
+          \param y The second dimension.
+          \param z The third dimension.
+      */
+      TH1 * createHistPlot(const std::string & title, const ValueSet & x, const ValueSet & y, const ValueSet & z);
+
+      /** \brief Create scatter plot as a Root object. Not part of API.
+          \param title The plot title.
+          \param x The first dimension.
+          \param y The second dimension.
+          \param z The third dimension.
+      */
+      TGraph * createScatterPlot(const std::string & title, const ValueSet & x, const ValueSet & y, const ValueSet & z);
+
     private:
-      IPlotFrame * m_parent;
+      IFrame * m_parent;
       TCanvas * m_canvas;
       TGraph * m_graph;
+      TH1 * m_hist;
   };
 }
 
