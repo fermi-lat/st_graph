80e23858ad411c23adcd61bb09d0e2e95959b49d
peachey <>
2004-10-12 15:53:38 +0000

    Throw exception if created engine could not initialize graphics.

--- src/Engine.cxx
+++ src/Engine.cxx
@@ -2,6 +2,8 @@
     \brief Implementation of class which encapsulates a particular graphics implementation.
     \author James Peachey, HEASARC/GSSC
 */
+#include <stdexcept>
+
 #include "RootEngine.h"
 #include "st_graph/Engine.h"
 
@@ -11,6 +13,8 @@ namespace st_graph {
 
   Engine & Engine::instance() {
     static RootEngine s_engine;
+    if (!s_engine.initSucceeded())
+      throw std::runtime_error("Engine::instance() could not initialize graphical environment");
     return s_engine;
   }
 
