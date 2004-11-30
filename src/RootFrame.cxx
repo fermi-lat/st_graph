f50af149f8a9a5552fe68ae59510bf6e48f82226
peachey <>
2004-11-30 18:29:09 +0000

    Consolidate two constructors, and add support for a RootFrame which can safely delete its parent.

--- src/RootFrame.cxx
+++ src/RootFrame.cxx
@@ -20,7 +20,8 @@ namespace st_graph {
     return &s_ancestor;
   }
 
-  RootFrame::RootFrame(IFrame * parent, TGFrame * frame): m_subframes(), m_parent(0), m_frame(frame), m_receiver(0) {
+  RootFrame::RootFrame(IFrame * parent, IEventReceiver * receiver, TGFrame * frame, bool delete_parent): m_subframes(),
+    m_parent(0), m_frame(frame), m_receiver(receiver), m_delete_parent(delete_parent) {
     // Make sure the parent is a Root parent.
     m_parent = dynamic_cast<RootFrame *>(parent);
     if (0 == m_parent)
@@ -29,29 +30,38 @@ namespace st_graph {
     m_parent->addFrame(this);
   }
 
-  RootFrame::RootFrame(IFrame * parent, IEventReceiver * receiver, TGFrame * frame): m_subframes(), m_parent(0), m_frame(frame),
-    m_receiver(receiver) {
-    // Make sure the parent is a Root parent.
-    m_parent = dynamic_cast<RootFrame *>(parent);
-    if (0 == m_parent)
-      throw std::logic_error("RootFrame constructor was passed a parent IFrame which is not a RootFrame");
-
-    m_parent->addFrame(this);
-  }
-
-  RootFrame::RootFrame(IEventReceiver * receiver, TGFrame * frame): m_subframes(), m_parent(RootFrame::ancestor()), m_frame(frame),
-    m_receiver(receiver) {
+  RootFrame::RootFrame(IEventReceiver * receiver, TGFrame * frame, bool delete_parent): m_subframes(),
+    m_parent(RootFrame::ancestor()), m_frame(frame), m_receiver(receiver), m_delete_parent(delete_parent) {
     m_parent->addFrame(this);
   }
 
   RootFrame::~RootFrame() {
+    // Note: This appears more complicated than necessary, but be careful changing it. Under some circumstances,
+    // a RootFrame needs to delete its parent, but the parent will always attempt to delete the RootFrame in the
+    // process. Thus it is important to ensure the child is detached at the right times to prevent deleting
+    // the parent and/or the child twice.
+
+    // Save pointer to parent in case delete parent was selected.
+    RootFrame * parent = m_parent;
+
+    // Break all connections between this and its parent.
     if (0 != m_parent) m_parent->removeFrame(this);
 
-    // Delete children. When they are deleted, they will remove themselves from the container of subframes.
-    while (!m_subframes.empty()) delete *--m_subframes.end();
+    // Delete children.
+    while (!m_subframes.empty()) {
+      // Find last child.
+      std::list<IFrame *>::iterator itor = --m_subframes.end();
+      // Break links between this and the child.
+      removeFrame(*itor);
+      // Delete the child.
+      delete *itor;
+    }
 
     // Delete the root frame.
     delete m_frame;
+
+    // In special case where the frame owns its parent frame, delete that as well.
+    if (m_delete_parent) delete parent;
   }
 
   void RootFrame::display() {
@@ -80,30 +90,30 @@ namespace st_graph {
 
       TGCompositeFrame * parent_tg_f = dynamic_cast<TGCompositeFrame *>(m_frame);
       if (0 != parent_tg_f && 0 != child_tg_f) parent_tg_f->AddFrame(child_tg_f);
-      m_subframes.push_back(frame);
+
+      // Connect this parent to the child.
+      rf->m_parent = this;
+
+      // Connect child to this parent.
+      m_subframes.push_back(rf);
     }
   }
 
   void RootFrame::removeFrame(IFrame * frame) {
-    // Make certain Root frame is not removed more than once.
     std::list<IFrame *>::iterator itor = std::find(m_subframes.begin(), m_subframes.end(), frame);
     if (m_subframes.end() != itor) {
+      // Disconnect child from parent.
       m_subframes.erase(itor);
       RootFrame * rf = dynamic_cast<RootFrame *>(frame);
       if (0 == rf) throw std::logic_error("RootFrame::removeFrame was passed an invalid child frame");
       TGFrame * child_tg_f = rf->getTGFrame();
 
+      // Disconnect parent from child.
+      rf->m_parent = 0;
+
       TGCompositeFrame * parent_tg_f = dynamic_cast<TGCompositeFrame *>(m_frame);
       if (0 != parent_tg_f && 0 != child_tg_f) parent_tg_f->RemoveFrame(child_tg_f);
     }
-  }
-
-  void RootFrame::clicked() {
-    m_receiver->clicked(this);
-  }
-
-  void RootFrame::closeWindow() {
-    m_receiver->closeWindow(this);
   }
 
   long RootFrame::getL() const {
@@ -125,6 +135,14 @@ namespace st_graph {
       r -= m_frame->GetX();
       if (r >= 0) m_frame->SetWidth(r);
     }
+  }
+
+  void RootFrame::clicked() {
+    m_receiver->clicked(this);
+  }
+
+  void RootFrame::closeWindow() {
+    m_receiver->closeWindow(this);
   }
 
   TGFrame * RootFrame::getTGFrame() { return m_frame; }
