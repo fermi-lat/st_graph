305f470b77f5ea1cc950cc497717e8d77a721e7d
peachey <>
2004-11-30 19:53:38 +0000

    Change signature of createPlot for self-contained plots so that it returns IPlot *. This uses the feature that IPlots can now own their parent frames. Also performed general cleanup.

--- src/RootEngine.cxx
+++ src/RootEngine.cxx
@@ -30,7 +30,7 @@ typedef void (*sighandler_t) (int);
 
 namespace st_graph {
 
-  RootEngine::RootEngine(): m_frames(), m_init_succeeded(false) {
+  RootEngine::RootEngine(): m_init_succeeded(false) {
     gSystem->ResetSignal(kSigBus);
     gSystem->ResetSignal(kSigSegmentationViolation);
     gSystem->ResetSignal(kSigSystem);
@@ -81,7 +81,6 @@ namespace st_graph {
 
     // Display all frames currently linked to the top-level frame.
     RootFrame::ancestor()->display();
-    for (FrameList_t::iterator itor = m_frames.begin(); itor != m_frames.end(); ++itor) (*itor)->display();
 
     // Run the Root event loop to handle the graphical displays.
     gApplication->Run(kTRUE);
@@ -90,7 +89,7 @@ namespace st_graph {
   void RootEngine::stop() {
     if (!m_init_succeeded) throw std::runtime_error("RootEngine::stop: graphical environment not initialized");
 
-    hideFrames();
+    RootFrame::ancestor()->unDisplay();
     gApplication->Terminate(0);
   }
 
@@ -126,40 +125,35 @@ namespace st_graph {
     // Connect appropriate Root Qt signals to this object's slot.
     tg_widget->Connect("CloseWindow()", "st_graph::RootFrame", frame, "closeWindow()");
 
-    //m_frames.push_back(frame);
     return frame;
   }
 
-  IFrame * RootEngine::createPlot(const std::string & title, unsigned int width, unsigned int height, const std::string & style,
+  IPlot * RootEngine::createPlot(const std::string & title, unsigned int width, unsigned int height, const std::string & style,
     const ISequence & x, const ISequence & y) {
     if (!m_init_succeeded) throw std::runtime_error("RootEngine::createPlot: graphical environment not initialized");
 
     // Create parent main frame.
     IFrame * mf = createMainFrame(0, width, height);
 
-    // Create frame to hold plot.
-    IFrame * pf = createPlotFrame(mf, title, width, height);
+    // Create frame to hold plot. This frame owns and will delete its parent.
+    IFrame * pf = new RootPlotFrame(mf, title, width, height, true);
 
-    // Create histogram plot. Discard return value to suppress warning. No memory leak because pf owns plot.
-    createPlot(pf, style, x, y);
-
-    return mf;
+    // Create plot. This plot owns and will delete its parent.
+    return new RootPlot(pf, style, x, y, true);
   }
 
-  IFrame * RootEngine::createPlot(const std::string & title, unsigned int width, unsigned int height, const std::string & style,
+  IPlot * RootEngine::createPlot(const std::string & title, unsigned int width, unsigned int height, const std::string & style,
     const ISequence & x, const ISequence & y, const std::vector<std::vector<double> > & z) {
     if (!m_init_succeeded) throw std::runtime_error("RootEngine::createPlot: graphical environment not initialized");
 
     // Create parent main frame.
     IFrame * mf = createMainFrame(0, width, height);
 
-    // Create frame to hold plot.
-    IFrame * pf = createPlotFrame(mf, title, width, height);
+    // Create frame to hold plot. This frame owns and will delete its parent.
+    IFrame * pf = new RootPlotFrame(mf, title, width, height, true);
 
-    // Create histogram plot. Discard return value to suppress warning. No memory leak because pf owns plot.
-    createPlot(pf, style, x, y, z);
-
-    return mf;
+    // Create plot. This plot owns and will delete its parent.
+    return new RootPlot(pf, style, x, y, z, true);
   }
 
   IPlot * RootEngine::createPlot(IFrame * parent, const std::string & style, const ISequence & x, const ISequence & y) {
@@ -208,26 +202,6 @@ namespace st_graph {
     tg_widget->Connect("Clicked()", "st_graph::RootFrame", frame, "clicked()");
 
     return frame;
-  }
-
-  void RootEngine::addFrame(IFrame * frame) {
-    if (!m_init_succeeded) throw std::runtime_error("RootEngine::addFrame: graphical environment not initialized");
-
-    m_frames.push_back(frame);
-  }
-
-  void RootEngine::removeFrame(IFrame * frame) {
-    if (!m_init_succeeded) throw std::runtime_error("RootEngine::removeFrame: graphical environment not initialized");
-
-    m_frames.remove(frame);
-  }
-
-  void RootEngine::hideFrames() {
-    if (!m_init_succeeded) throw std::runtime_error("RootEngine::hideFrames: graphical environment not initialized");
-
-    for (FrameList_t::iterator itor = m_frames.begin(); itor != m_frames.end(); ++itor) (*itor)->unDisplay();
-    // Hide all frames currently linked to the top-level frame.
-    RootFrame::ancestor()->unDisplay();
   }
 
 }
--- src/RootEngine.h
+++ src/RootEngine.h
@@ -21,9 +21,6 @@ namespace st_graph {
   */
   class RootEngine : public Engine {
     public:
-      /// \brief Container of plots registered with this engine.
-      typedef std::list<IFrame *> FrameList_t;
-
       /// \brief Create the Root graphics engine. Creates a Root TApplication object.
       RootEngine();
 
@@ -43,7 +40,7 @@ namespace st_graph {
           \param x The first dimension being plotted, giving the bin definitions.
           \param y The second dimension being plotted, giving the bin values.
       */
-      virtual IFrame * createPlot(const std::string & title, unsigned int width, unsigned int height, const std::string & style,
+      virtual IPlot * createPlot(const std::string & title, unsigned int width, unsigned int height, const std::string & style,
         const ISequence & x, const ISequence & y);
 
       /** \brief Create a self-contained three dimensional plot window.
@@ -55,10 +52,10 @@ namespace st_graph {
           \param y The second dimension being plotted, giving the y bin definitions.
           \param z The third dimension being plotted.
       */
-      virtual IFrame * createPlot(const std::string & title, unsigned int width, unsigned int height, const std::string & style,
+      virtual IPlot * createPlot(const std::string & title, unsigned int width, unsigned int height, const std::string & style,
         const ISequence & x, const ISequence & y, const std::vector<std::vector<double> > & z);
 
-      /** \brief Create a top-level frame on the desktop. This is the first window which should be created.
+      /** \brief Create a top-level independent frame on the desktop. This frame's purpose is to hold other frames.
           \param receiver The receiver of GUI signals.
           \param width The width of the plot window.
           \param height The height of the plot window.
@@ -66,7 +63,8 @@ namespace st_graph {
       virtual IFrame * createMainFrame(IEventReceiver * receiver, unsigned int width, unsigned int height);
 
       /** \brief Create a plot which may be displayed in a plot frame.
-          \param parent The parent frame in which the plot will be displayed.
+          \param parent The parent frame in which the plot will be displayed. This must have been created by
+                 createPlotFrame.
           \param style The plot style: currently hist* or scat* will be recognized, case insensitive, to mean
                  histogram or scatter plot, respectively.
           \param x The first dimension being plotted.
@@ -75,7 +73,8 @@ namespace st_graph {
       virtual IPlot * createPlot(IFrame * parent, const std::string & style, const ISequence & x, const ISequence & y);
 
       /** \brief Create a plot which may be displayed in a plot frame.
-          \param parent The parent frame in which the plot will be displayed.
+          \param parent The parent frame in which the plot will be displayed. This must have been created by
+                 createPlotFrame.
           \param style The plot style:
           \param x The first dimension being plotted.
           \param y The second dimension being plotted.
@@ -84,8 +83,7 @@ namespace st_graph {
       virtual IPlot * createPlot(IFrame * parent, const std::string & style, const ISequence & x, const ISequence & y,
         const std::vector<std::vector<double> > & z);
 
-      /** \brief Create a frame specifically devoted to holding plots. This method will be removed shortly,
-                 because a new technique is planned to support plots within any frame.
+      /** \brief Create a frame specifically devoted to holding plots.
           \param parent The frame in which to embed the plot frame.
           \param title The title of the plot.
           \param width The width of the frame in pixels.
@@ -93,29 +91,19 @@ namespace st_graph {
       */
       virtual IFrame * createPlotFrame(IFrame * parent, const std::string & title, unsigned int width, unsigned int height);
 
+      /** \brief Create a button whose events are bound to the given event receiver object.
+          \param parent The frame in which to embed the button.
+          \param receiver The event receiver which will process events from the button (clicks etc.)
+          \param style The style of button, e.g. text, radio, etc.
+          \param label The label appearing on the button.
+      */
       virtual IFrame * createButton(IFrame * parent, IEventReceiver * receiver, const std::string & style,
         const std::string & label);
-
-      /** \brief Register a frame with the engine. This allows the engine to display or hide
-          its associated graphical frames (Root-implementation-specific)
-          \param frame The graphical frame to register.
-      */
-      virtual void addFrame(IFrame * frame);
-
-      /** \brief Unregister a frame with the engine. Following a call to this, the given frame will
-          not be displayed or hidden when the engine runs. (Root-implementation-specific)
-          \param frame The graphical frame to unregister.
-      */
-      virtual void removeFrame(IFrame * frame);
-
-      /// \brief Hide all frames from view.
-      virtual void hideFrames();
 
       /// \brief Return whether graphics engine was successfully initialized.
       virtual bool initSucceeded() const { return m_init_succeeded; }
 
     private:
-      FrameList_t m_frames;
       bool m_init_succeeded;
   };
 
