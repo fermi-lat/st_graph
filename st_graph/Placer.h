acb9ada1955e648d8176af6e4777378f64922c6b
peachey <>
2004-12-29 21:04:20 +0000

    Generalize placer objects to handle both hvertical as well as horizontal placements.

--- st_graph/Placer.h
+++ st_graph/Placer.h
@@ -1,5 +1,5 @@
 /** \file Placer.h
-    \brief Interface for Placer class.
+    \brief Interface for HPlacer and VPlacer classes.
     \author James Peachey, HEASARC/GSSC
 */
 #ifndef st_graph_Placer_h
@@ -9,53 +9,146 @@ namespace st_graph {
 
   class IFrame;
 
-  /** \class Placer
+  /** \class HPlacer
   */
-  class Placer {
+  class HPlacer {
     public:
-      Placer(IFrame * frame): m_frame(frame) {}
+      HPlacer(IFrame * frame): m_frame(frame) {}
 
-      virtual ~Placer() {}
+      virtual ~HPlacer() {}
 
-      virtual long get() const = 0;
+      virtual long getH() const = 0;
+
+      virtual void adopt(IFrame * frame) { m_frame = frame; }
 
     protected:
       IFrame * m_frame;
   };
 
-#if 0
-  class Center : public Placer {
+  /** \class VPlacer
+  */
+  class VPlacer {
     public:
-      Center(IFrame * frame): Placer(frame) {}
+      VPlacer(IFrame * frame): m_frame(frame) {}
 
-      IFrame * leftOf(Placer & neighbor, long offset = 0) { m_frame->setHCenter(neighbor.get() - offset); return m_frame; }
+      virtual ~VPlacer() {}
 
-      IFrame * rightOf(Placer & neighbor, long offset = 0) { m_frame->setHCenter(neighbor.get() + offset); return m_frame; }
+      virtual long getV() const = 0;
 
-      virtual long get() const { return m_frame->getHCenter(); }
-  };
-#endif
+      virtual void adopt(IFrame * frame) { m_frame = frame; }
 
-  class LeftEdge : public Placer {
-    public:
-      LeftEdge(IFrame * frame): Placer(frame) {}
-
-      IFrame * leftOf(Placer & neighbor, long offset = 0) { m_frame->setL(neighbor.get() - offset); return m_frame; }
-
-      IFrame * rightOf(Placer & neighbor, long offset = 0) { m_frame->setL(neighbor.get() + offset); return m_frame; }
-
-      virtual long get() const { return m_frame->getL(); }
+    protected:
+      IFrame * m_frame;
   };
 
-  class RightEdge : public Placer {
+  class Center : public HPlacer, public VPlacer {
     public:
-      RightEdge(IFrame * frame): Placer(frame) {}
+      Center(IFrame * frame): HPlacer(frame), VPlacer(frame) {}
 
-      IFrame * leftOf(Placer & neighbor, long offset = 0) { m_frame->setR(neighbor.get() - offset); return m_frame; }
+      IFrame * above(const VPlacer & neighbor, long offset = 0) {
+        VPlacer::m_frame->setVCenter(neighbor.getV() - offset);
+        return VPlacer::m_frame;
+      }
 
-      IFrame * rightOf(Placer & neighbor, long offset = 0) { m_frame->setR(neighbor.get() + offset); return m_frame; }
+      IFrame * below(const VPlacer & neighbor, long offset = 0) {
+        VPlacer::m_frame->setVCenter(neighbor.getV() + offset);
+        return VPlacer::m_frame;
+      }
 
-      virtual long get() const { return m_frame->getR(); }
+      IFrame * leftOf(const HPlacer & neighbor, long offset = 0) {
+        HPlacer::m_frame->setHCenter(neighbor.getH() - offset);
+        return HPlacer::m_frame;
+      }
+
+      IFrame * rightOf(const HPlacer & neighbor, long offset = 0) {
+        HPlacer::m_frame->setHCenter(neighbor.getH() + offset);
+        return HPlacer::m_frame;
+      }
+
+      virtual long getH() const { return HPlacer::m_frame->getHCenter(); }
+
+      virtual long getV() const { return VPlacer::m_frame->getVCenter(); }
+
+      virtual void adopt(IFrame * frame) { HPlacer::adopt(frame); VPlacer::adopt(frame); }
+
+  };
+
+  class LeftEdge : public HPlacer {
+    public:
+      LeftEdge(IFrame * frame): HPlacer(frame) {}
+
+      IFrame * leftOf(const HPlacer & neighbor, long offset = 0) { m_frame->setL(neighbor.getH() - offset); return m_frame; }
+
+      IFrame * rightOf(const HPlacer & neighbor, long offset = 0) { m_frame->setL(neighbor.getH() + offset); return m_frame; }
+
+      IFrame * stretchTo(const HPlacer & neighbor, long offset = 0) {
+        long edge = neighbor.getH() + offset;
+        edge = edge > 0 ? edge : 0;
+        long size = m_frame->getR() - edge;
+        if (0 <= size) {
+          m_frame->setL(edge);
+          m_frame->setWidth(size);
+        }
+        return m_frame;
+      }
+
+      virtual long getH() const { return m_frame->getL(); }
+  };
+
+  class RightEdge : public HPlacer {
+    public:
+      RightEdge(IFrame * frame): HPlacer(frame) {}
+
+      IFrame * leftOf(const HPlacer & neighbor, long offset = 0) { m_frame->setR(neighbor.getH() - offset); return m_frame; }
+
+      IFrame * rightOf(const HPlacer & neighbor, long offset = 0) { m_frame->setR(neighbor.getH() + offset); return m_frame; }
+
+      IFrame * stretchTo(const HPlacer & neighbor, long offset = 0) {
+        long size = neighbor.getH() + offset - m_frame->getL();
+        if (0 <= size) m_frame->setWidth(size);
+        return m_frame;
+      }
+
+      virtual long getH() const { return m_frame->getR(); }
+  };
+
+  class TopEdge : public VPlacer {
+    public:
+      TopEdge(IFrame * frame): VPlacer(frame) {}
+
+      IFrame * above(const VPlacer & neighbor, long offset = 0) { m_frame->setT(neighbor.getV() - offset); return m_frame; }
+
+      IFrame * below(const VPlacer & neighbor, long offset = 0) { m_frame->setT(neighbor.getV() + offset); return m_frame; }
+
+      IFrame * stretchTo(const VPlacer & neighbor, long offset = 0) {
+        long edge = neighbor.getV() + offset;
+        edge = edge > 0 ? edge : 0;
+        long size = m_frame->getB() - edge;
+        if (0 <= size) {
+          m_frame->setT(edge);
+          m_frame->setHeight(size);
+        }
+        return m_frame;
+      }
+
+      virtual long getV() const { return m_frame->getT(); }
+  };
+
+  class BottomEdge : public VPlacer {
+    public:
+      BottomEdge(IFrame * frame): VPlacer(frame) {}
+
+      IFrame * above(const VPlacer & neighbor, long offset = 0) { m_frame->setB(neighbor.getV() - offset); return m_frame; }
+
+      IFrame * below(const VPlacer & neighbor, long offset = 0) { m_frame->setB(neighbor.getV() + offset); return m_frame; }
+
+      IFrame * stretchTo(const VPlacer & neighbor, long offset = 0) {
+        long size = neighbor.getV() + offset - m_frame->getT();
+        if (0 <= size) m_frame->setHeight(size);
+        return m_frame;
+      }
+
+      virtual long getV() const { return m_frame->getB(); }
   };
 
 }
