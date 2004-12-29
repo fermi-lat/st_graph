b4c00e78bdbd62db70db6ba526f95b0b3bc1a4a9
peachey <>
2004-12-29 20:59:32 +0000

    Be more precise with regard to how changes of size and calls to Layout take place.

--- src/STGLayoutManager.h
+++ src/STGLayoutManager.h
@@ -1,5 +1,5 @@
 /** \file STGLayoutManager.cxx
-    \brief Modified TGLayoutManager for internal use by IFrames which contain other frames.
+    \brief Modified TGLayoutManager for internal use by RootFrames which contain other frames.
     \author James Peachey, HEASARC/GSSC
 */
 #include "TCollection.h"
@@ -7,15 +7,15 @@
 #include "TGFrame.h"
 #include "TGLayout.h"
 
+#include "RootFrame.h"
 #include "st_graph/IEventReceiver.h"
-#include "st_graph/IFrame.h"
 
 namespace st_graph {
 
   class STGLayoutManager : public TGLayoutManager {
     public:
-      STGLayoutManager(IEventReceiver * receiver, IFrame * frame, TGCompositeFrame * tg_frame):
-        m_receiver(receiver), m_frame(frame), m_tg_frame(tg_frame), m_default_size(frame->getWidth(), frame->getHeight()) {}
+      STGLayoutManager(IEventReceiver * receiver, RootFrame * frame, TGCompositeFrame * tg_frame):
+        m_receiver(receiver), m_frame(frame), m_tg_frame(tg_frame), m_default_size(frame->getTGFrame()->GetSize()) {}
 
       virtual void Layout() {
         // Lay out the parent frame using the receiver.
@@ -24,15 +24,16 @@ namespace st_graph {
         // Lay out each child using its native TGLayoutManager.
         TIter next(m_tg_frame->GetList());
         while(TGFrameElement * el = dynamic_cast<TGFrameElement *>(next())) {
-          el->fFrame->Layout();
+          el->fFrame->MoveResize(el->fFrame->GetX(), el->fFrame->GetY(), el->fFrame->GetWidth(), el->fFrame->GetHeight());
         }
+
       }
 
       virtual TGDimension GetDefaultSize() const { return m_default_size; }
 
     private:
       IEventReceiver * m_receiver;
-      IFrame * m_frame;
+      RootFrame * m_frame;
       TGCompositeFrame * m_tg_frame;
       TGDimension m_default_size;
   }; 
