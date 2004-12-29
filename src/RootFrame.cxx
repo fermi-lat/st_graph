aea4e8088b53749aab5b43a36de7d84ad8d9fe57
peachey <>
2004-12-29 20:55:16 +0000

    Add full set of methods for positioning widgets. Add minimum width and height. Change modifications in text entry so that they take effect immediately.

--- src/RootFrame.cxx
+++ src/RootFrame.cxx
@@ -6,6 +6,7 @@
 #include <stdexcept>
 
 #include "TGFrame.h"
+#include "TGLayout.h"
 
 #include "st_graph/IEventReceiver.h"
 
@@ -21,7 +22,7 @@ namespace st_graph {
   }
 
   RootFrame::RootFrame(IFrame * parent, IEventReceiver * receiver, TGFrame * frame, bool delete_parent): m_subframes(),
-    m_parent(0), m_frame(frame), m_receiver(receiver), m_delete_parent(delete_parent) {
+    m_parent(0), m_frame(frame), m_receiver(receiver), m_delete_parent(delete_parent), m_minimum_width(0), m_minimum_height(0) {
     // Make sure the parent is a Root parent.
     m_parent = dynamic_cast<RootFrame *>(parent);
     if (0 == m_parent)
@@ -31,7 +32,8 @@ namespace st_graph {
   }
 
   RootFrame::RootFrame(IEventReceiver * receiver, TGFrame * frame, bool delete_parent): m_subframes(),
-    m_parent(RootFrame::ancestor()), m_frame(frame), m_receiver(receiver), m_delete_parent(delete_parent) {
+    m_parent(RootFrame::ancestor()), m_frame(frame), m_receiver(receiver), m_delete_parent(delete_parent),
+    m_minimum_width(0), m_minimum_height(0) {
     m_parent->addFrame(this);
   }
 
@@ -50,7 +52,7 @@ namespace st_graph {
     // Delete children.
     while (!m_subframes.empty()) {
       // Find last child.
-      std::list<IFrame *>::iterator itor = --m_subframes.end();
+      std::list<RootFrame *>::iterator itor = --m_subframes.end();
       // Break links between this and the child.
       removeFrame(*itor);
       // Delete the child.
@@ -65,19 +67,19 @@ namespace st_graph {
   }
 
   void RootFrame::display() {
-    for (std::list<IFrame *>::iterator itor = m_subframes.begin(); itor != m_subframes.end(); ++itor)
-      (*itor)->display();
     if (0 != m_frame) {
       m_frame->MapSubwindows();
       m_frame->Layout();
       m_frame->MapWindow();
     }
+    for (std::list<RootFrame *>::iterator itor = m_subframes.begin(); itor != m_subframes.end(); ++itor)
+      (*itor)->display();
   }
 
   void RootFrame::unDisplay() {
     if (0 != m_frame)
       m_frame->UnmapWindow();
-    for (std::list<IFrame *>::reverse_iterator itor = m_subframes.rbegin(); itor != m_subframes.rend(); ++itor)
+    for (std::list<RootFrame *>::reverse_iterator itor = m_subframes.rbegin(); itor != m_subframes.rend(); ++itor)
       (*itor)->unDisplay();
   }
 
@@ -100,7 +102,7 @@ namespace st_graph {
   }
 
   void RootFrame::removeFrame(IFrame * frame) {
-    std::list<IFrame *>::iterator itor = std::find(m_subframes.begin(), m_subframes.end(), frame);
+    std::list<RootFrame *>::iterator itor = std::find(m_subframes.begin(), m_subframes.end(), frame);
     if (m_subframes.end() != itor) {
       // Disconnect child from parent.
       m_subframes.erase(itor);
@@ -116,14 +118,64 @@ namespace st_graph {
     }
   }
 
+  /// \brief Get the horizontal center of the frame.
+  long RootFrame::getHCenter() const {
+    long center = 0;
+    if (0 != m_frame) {
+      // Computation designed to minimize round-off error.
+      long left = m_frame->GetX();
+      // center = left + .5 * width = .5 * (2. * left + width) = (left + left + width) / 2
+      center = (left + left + m_frame->GetWidth()) / 2;
+    }
+    return center;
+  }
+
+  /// \brief Set the horizontal center of the frame.
+  void RootFrame::setHCenter(long center) {
+    if (0 != m_frame) {
+      // Computation designed to minimize round-off error.
+      long width = m_frame->GetWidth();
+      // left = center - .5 * width = .5 * (2. * center - width) = (center + center - width) / 2
+      long left = (center + center - width) / 2;
+      left = left > 0 ? left : 0;
+      m_frame->Move(left, m_frame->GetY());
+    }
+  }
+
+  long RootFrame::getVCenter() const {
+    long center = 0;
+    if (0 != m_frame) {
+      // Computation designed to minimize round-off error.
+      long top = m_frame->GetY();
+      if (0 != m_parent) top += m_parent->getT(); // Convert to absolute coordinates.
+      // center = top + .5 * height = .5 * (2. * top + height) = (top + top + width) / 2
+      center = (top + top + m_frame->GetHeight()) / 2;
+    }
+    return center;
+  }
+
+  void RootFrame::setVCenter(long center) {
+    if (0 != m_frame) {
+      // Computation designed to minimize round-off error.
+      long height = m_frame->GetHeight();
+      // top = center - .5 * height = .5 * (2. * center - height) = (center + center - height) / 2
+      long top = (center + center - height) / 2;
+      if (0 != m_parent) top -= m_parent->getT(); // Convert to relative coordinates.
+      top = top > 0 ? top: 0;
+      m_frame->Move(m_frame->GetX(), top);
+    }
+  }
+
   long RootFrame::getL() const {
     long l = 0;
     if (0 != m_frame) l = m_frame->GetX();
+    if (0 != m_parent) l += m_parent->getL(); // Convert to absolute coordinates.
     return l;
   }
 
   void RootFrame::setL(long l) {
     if (0 != m_frame) {
+      if (0 != m_parent) l -= m_parent->getL(); // Convert to relative coordinates.
       l = l > 0 ? l : 0;
       m_frame->Move(l, m_frame->GetY());
     }
@@ -132,20 +184,81 @@ namespace st_graph {
   long RootFrame::getR() const {
     long r = 0;
     if (0 != m_frame) r = m_frame->GetX() + m_frame->GetWidth();
+    if (0 != m_parent) r += m_parent->getL(); // Convert to absolute coordinates.
     return r;
   }
 
   void RootFrame::setR(long r) {
     if (0 != m_frame) {
-      r -= m_frame->GetWidth();
-      r = r > 0 ? r : 0;
-      m_frame->Move(r, m_frame->GetY());
+      long l = r - m_frame->GetWidth();
+      if (0 != m_parent) l -= m_parent->getL(); // Convert to relative coordinates.
+      l = l > 0 ? l : 0;
+      m_frame->Move(l, m_frame->GetY());
     }
   }
 
-  long RootFrame::getWidth() const { return m_frame->GetWidth(); }
+  long RootFrame::getT() const {
+    long t = 0;
+    if (0 != m_frame) t = m_frame->GetY();
+    if (0 != m_parent) t += m_parent->getT(); // Convert to absolute coordinates.
+    return t;
+  }
 
-  long RootFrame::getHeight() const { return m_frame->GetHeight(); }
+  void RootFrame::setT(long t) {
+    if (0 != m_frame) {
+      if (0 != m_parent) t -= m_parent->getT(); // Convert to relative coordinates.
+      t = t > 0 ? t : 0;
+      m_frame->Move(m_frame->GetX(), t);
+    }
+  }
+
+  long RootFrame::getB() const {
+    long b = 0;
+    if (0 != m_frame) b = m_frame->GetY() + m_frame->GetHeight();
+    if (0 != m_parent) b += m_parent->getT(); // Convert to absolute coordinates.
+    return b;
+  }
+
+  void RootFrame::setB(long b) {
+    if (0 != m_frame) {
+      long t = b - m_frame->GetHeight();
+      if (0 != m_parent) t -= m_parent->getT(); // Convert to relative coordinates.
+      t = t > 0 ? t : 0;
+      m_frame->Move(m_frame->GetX(), t);
+    }
+  }
+
+  long RootFrame::getWidth() const {
+    long width = 0;
+    if (0 != m_frame) width = m_frame->GetWidth();
+    return width;
+  }
+
+  void RootFrame::setWidth(long width) {
+    width = (m_minimum_width < width) ? width : m_minimum_width;
+    if (0 != m_frame)
+      m_frame->SetWidth(width);
+  }
+
+  long RootFrame::getHeight() const {
+    long height = 0;
+    if (0 != m_frame) height = m_frame->GetHeight();
+    return height;
+  }
+
+  void RootFrame::setHeight(long height) {
+    height = (m_minimum_height < height) ? height : m_minimum_height;
+    if (0 != m_frame)
+      m_frame->SetHeight(height);
+  }
+
+  long RootFrame::getMinimumWidth() const { return m_minimum_width; }
+
+  void RootFrame::setMinimumWidth(long width) { m_minimum_width = width > 0 ? width : 0; }
+
+  long RootFrame::getMinimumHeight() const { return m_minimum_height; }
+
+  void RootFrame::setMinimumHeight(long height) { m_minimum_height = height > 0 ? height : 0; }
 
   void RootFrame::clicked() {
     m_receiver->clicked(this);
@@ -153,6 +266,10 @@ namespace st_graph {
 
   void RootFrame::closeWindow() {
     m_receiver->closeWindow(this);
+  }
+
+  void RootFrame::modified(const char * text) {
+    m_receiver->modified(this, text);
   }
 
   TGFrame * RootFrame::getTGFrame() { return m_frame; }
